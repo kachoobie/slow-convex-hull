b719c8f0802510c1eba21cca94d82a441eeee1df
kachoobie <realkachoobie@gmail.com>
2025-08-30 07:10:00 -1000

    Remove some files

--- compile.py
+++ compile.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("clang++ -std=c++20 -o exec main.cpp src/*.cpp -I include `pkg-config --libs --cflags sdl3`")
+os.system("clang++ -std=c++20 -o exec.out main.cpp src/*.cpp -I include `pkg-config --libs --cflags sdl3`")
